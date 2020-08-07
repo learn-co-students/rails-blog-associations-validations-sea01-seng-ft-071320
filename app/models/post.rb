cdd1101e0fad24ef151a42133e95adb3b10650dc
afteralec <github email address>
2020-08-07 10:27:12 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :post_tags
   has_many :tags, through: :post_tags
+  # accepts_nested_attributes_for :tags
 
   validates :name, :content, presence: true
-  
 end
